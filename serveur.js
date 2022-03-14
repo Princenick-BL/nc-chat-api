3d3a8dc4c4275c1248a699718299114e107fccef
Princenick-BL <princenickballo@gmail.com>
2022-03-14 20:58:11 +0100

    cors

--- serveur.js
+++ serveur.js
@@ -179,7 +179,7 @@ app.post("/messages", async (req, res) => {
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true,
